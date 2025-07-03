8ea512a4bfb53d1eb2aaf26509aef45c19261d2e
Washington Santos <washinsantos907@gmail.com>
2025-07-03 18:16:08 -0300

    Primeiro ex

---
+++ ex036.py
@@ -0,0 +1,15 @@
+#Escreva um programa para aprovar um empréstimo bancário para comprar uma casa. Perguntando ao cliente, 
+#valor da casa, o salario, e em quantos anos ele quer pagar, a prestação mensal não pode exceder 30%
+#do salario, se não o empréstimo será negado !
+
+valor_casa = float(input("Digite o valor da casa: "))
+salario = float(input("Digite o seu salario: "))
+anos = int(input("Quantos anos deseja pagar? "))
+parcela_mensal = valor_casa / (anos * 12)
+
+porcentagem_minima = salario*0.3 # Ou salario * 30 / 100
+
+if parcela_mensal > porcentagem_minima:
+    print("Foi NEGADO pois a parcela é de {} e 30% do seu salario é {}".format(parcela_mensal, porcentagem_minima))
+else:
+    print("Foi APROVADO, com uma parcela de {}".format(parcela_mensal))
