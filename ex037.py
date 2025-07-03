d46cd6d661a20d890f281010f228b11d7b86f8a4
Washington Santos <washinsantos907@gmail.com>
2025-07-03 19:31:38 -0300

    add Com ou sem Prefixo

--- ex037.py
+++ ex037.py
@@ -15,10 +15,25 @@ opcao = int(input("Digite a conversão que deseja fazer: "))
 while opcao > 3 or opcao < 1:
     opcao = int(input("Escolha somente uma das 3 opções: "))
 
+print(''' 
+[0] Resposta com Prefixo
+[1] Resposta sem Prefixo
+''')
+opcao2 = int(input("Digite 0 ou 1: "))
 
-if opcao == 1:
-    print("Seu numero em binario é {}".format(bin(numero)[2:])) #Caso queira pegar o resultado depois do "0b" print("Seu numero em binario é {}".format(bin(numero)[2:]))
-elif opcao == 2:
-    print("Seu numero em octal é {}".format(oct(numero))) #Caso queira pegar o resultado depois do "0o" print("Seu numero em octal é {}".format(oct(numero)[2:]))
+if opcao2 == 0:
+    if opcao == 1:
+        print("Seu numero em binario é {}".format(bin(numero)))
+    elif opcao == 2:
+        print("Seu numero em octal é {}".format(oct(numero))) 
+    else:
+        print(f"Seu numero em hexadecimal é {hex(numero)}") 
 else:
-    print(f"Seu numero em hexadecimal é {hex(numero)}") #Caso queira pegar o resultado depois do "0x"  print(f"Seu numero em hexadecimal é {hex(numero)[2:]}")
+    if opcao == 1:
+        print("Seu numero em binario é {}".format(bin(numero)[2:])) #Caso queira pegar o resultado depois do "0b" print("Seu numero em binario é {}".format(bin(numero)[2:]))
+    elif opcao == 2:
+        print("Seu numero em octal é {}".format(oct(numero)[2:])) #Caso queira pegar o resultado depois do "0o" print("Seu numero em octal é {}".format(oct(numero)[2:]))
+    else:
+        print(f"Seu numero em hexadecimal é {hex(numero)[2:]}") #Caso queira pegar o resultado depois do "0x"  print(f"Seu numero em hexadecimal é {hex(numero)[2:]}")
+
+
